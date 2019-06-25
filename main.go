efba77b89bf27cc481a6fc10e8854db38106e1e1
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2019-06-25 20:18:06 +0200

    authentification sur keycloak

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
+	"strings"
 
+	"github.com/Nerzal/gocloak"
 	"github.com/tus/tusd"
 	"github.com/tus/tusd/filestore"
 
-	jwt "github.com/appleboy/gin-jwt"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -30,82 +31,7 @@ type File struct {
 	Tags   []string `json:"tags"`
 }
 
-func payloadHandler(data interface{}) jwt.MapClaims {
-	if v, ok := data.(*Payload); ok {
-		return jwt.MapClaims{
-			"email": v.Email,
-			"scope": v.Scope,
-			"value": v.Value,
-		}
-	}
-	return jwt.MapClaims{}
-}
-
-func identityHandler(c *gin.Context) interface{} {
-	claims := jwt.ExtractClaims(c)
-	email, ok := claims["email"].(string)
-	if !ok {
-		return nil
-	}
-
-	user := email
-
-	return &user
-}
-
-func authorizatorHandler(data interface{}, c *gin.Context) bool {
-	return true
-}
-
-func unauthorizedHandler(c *gin.Context, code int, message string) {
-	c.JSON(code, gin.H{
-		"code":    code,
-		"message": message,
-	})
-}
-
-// authenticator est une fonction temporaire pour assurer l'authentification pour la démonstration qui ne restera pas dans la version production
-func authenticator(c *gin.Context) (interface{}, error) {
-	var loginVals struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-
-	if err := c.ShouldBind(&loginVals); err != nil {
-		return "", jwt.ErrMissingLoginValues
-	}
-	userID := loginVals.Email
-	password := loginVals.Password
-
-	if userID == "user1@test.com" && password == "upload" {
-		return &Payload{
-			Email: loginVals.Email,
-			Scope: []string{"user1", "BFC", "stats"},
-			Value: map[string]interface{}{
-				"goup-path": "user1",
-			},
-		}, nil
-	}
-
-	if userID == "user2@test.com" && password == "upload" {
-		return &Payload{
-			Email: loginVals.Email,
-			Scope: []string{"user2", "BFC", "stats"},
-			Value: map[string]interface{}{
-				"goup-path": "user2",
-			},
-		}, nil
-	}
-
-	if userID == "user3@test.com" && password == "noupload" {
-		return &Payload{
-			Email: loginVals.Email,
-			Scope: []string{"user3", "PDL"},
-		}, nil
-	}
-
-	return nil, jwt.ErrFailedAuthentication
-}
+var keycloak gocloak.GoCloak
 
 func processUpload() chan tusd.FileInfo {
 	channel := make(chan tusd.FileInfo)
@@ -122,23 +48,10 @@ func processUpload() chan tusd.FileInfo {
 
 func addMetadata(c *gin.Context) {
 	metadata := c.Request.Header.Get("upload-metadata")
-	claims := jwt.ExtractClaims(c)
+	claims := c.Keys["claims"].(*jwt.MapClaims)
 
-	value, ok := claims["value"].(map[string]interface{})
-	if !ok {
-		c.JSON(403, "Forbidden")
-		c.Abort()
-		return
-	}
-
-	pathInterface, ok := value["goup-path"]
-	if !ok {
-		c.JSON(403, "Forbidden")
-		c.Abort()
-		return
-	}
-
-	path, ok := pathInterface.(string)
+	fmt.Println("coucou")
+	path, ok := (*claims)["goup_path"].(string)
 	if !ok {
 		c.JSON(403, "Forbidden")
 		c.Abort()
@@ -171,6 +84,8 @@ func main() {
 		panic("Absence du répertoire de base")
 	}
 
+	keycloak = gocloak.NewClient(viper.GetString("keycloakHostname"))
+
 	store := filestore.FileStore{
 		Path: viper.GetString("basePath") + "/tusd/",
 	}
@@ -191,33 +106,15 @@ func main() {
 		panic(fmt.Errorf("Unable to create handler: %s", err))
 	}
 
-	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-		Realm:           "Signaux-Faibles",
-		Key:             []byte(viper.GetString("jwtSecret")),
-		SendCookie:      false,
-		Timeout:         time.Hour,
-		MaxRefresh:      time.Hour,
-		IdentityKey:     "id",
-		PayloadFunc:     payloadHandler,
-		IdentityHandler: identityHandler,
-		Authenticator:   authenticator,
-		Authorizator:    authorizatorHandler,
-		Unauthorized:    unauthorizedHandler,
-		TokenLookup:     "header: Authorization, query: token",
-		TokenHeadName:   "Bearer",
-		TimeFunc:        time.Now,
-	})
-
 	hostname := viper.GetString("hostname")
 	router := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{hostname}
 	config.AddAllowHeaders("Authorization", "tus-resumable", "upload-length", "upload-metadata", "upload-offset", "Location")
 	router.Use(cors.New(config))
-	router.POST("/login", authMiddleware.LoginHandler)
-	router.POST("/files/*any", authMiddleware.MiddlewareFunc(), addMetadata, gin.WrapH(http.StripPrefix("/files/", handler)))
-	router.HEAD("/files/*any", authMiddleware.MiddlewareFunc(), gin.WrapH(http.StripPrefix("/files/", handler)))
-	router.PATCH("/files/*any", authMiddleware.MiddlewareFunc(), gin.WrapH(http.StripPrefix("/files/", handler)))
+	router.POST("/files/*any", keycloakMiddleware, addMetadata, gin.WrapH(http.StripPrefix("/files/", handler)))
+	router.HEAD("/files/*any", keycloakMiddleware, gin.WrapH(http.StripPrefix("/files/", handler)))
+	router.PATCH("/files/*any", keycloakMiddleware, gin.WrapH(http.StripPrefix("/files/", handler)))
 	bind := viper.GetString("bind")
 	router.Run(bind)
 }
@@ -229,4 +126,32 @@ func isDirectory(path string) bool {
 		return true
 	}
 	return false
+}
+
+func keycloakMiddleware(c *gin.Context) {
+	header := c.Request.Header["Authorization"][0]
+
+	rawToken := strings.Split(header, " ")[1]
+
+	token, claims, err := keycloak.DecodeAccessToken(rawToken, viper.GetString("keycloakRealm"))
+	if errValid := claims.Valid(); err != nil && errValid != nil {
+		c.AbortWithStatus(401)
+	}
+
+	c.Set("token", token)
+	c.Set("claims", claims)
+
+	c.Next()
+}
+
+func scopeFromClaims(claims *jwt.MapClaims) []string {
+	resourceAccess := (*claims)["resource_access"].(map[string]interface{})
+	client := (resourceAccess)["signauxfaibles"].(map[string]interface{})
+	scope := (client)["roles"].([]interface{})
+
+	var tags []string
+	for _, tag := range scope {
+		tags = append(tags, tag.(string))
+	}
+	return tags
 }
