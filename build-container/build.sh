13ac02a8268456cdee49233ab994c1d34c1fe1e0
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2020-06-18 10:03:42 +0200

    migration go modules

---
+++ build-container/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "build.sh: construit l'application goup dans une image docker"
+    echo "usage: build.sh branch"
+    echo "exemple: ./build.sh master"
+    exit 255
+fi
+
+if [ -d workspace ]; then
+    echo "supprimer le répertoire workspace avant de commencer"
+    exit 1
+fi
+
+# Checkout git
+mkdir workspace
+cd workspace
+curl -LOs "https://github.com/signaux-faibles/goup/archive/$1.zip"
+
+if [ $(openssl dgst -md5 $1.zip |awk '{print $2}') = '3be7b8b182ccd96e48989b4e57311193' ]; then
+   echo "sources manquantes, branche probablement inexistante"
+   exit
+fi
+
+# Unzip des sources et build
+unzip "$1.zip"
+cd "goup-$1"
+
+CGO_ENABLED=0 go build
+
+# Build docker
+cd ../..
+docker build -t goup --build-arg binary="./workspace/goup-$1/goup" . 
+docker save goup | gzip > goup.tar.gz
+
+# Cleanup
+rm -rf workspace
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/signaux-faibles/goup
+
+go 1.14
+
+require (
+	github.com/Nerzal/gocloak v1.0.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.6.3
+	github.com/spf13/viper v1.7.0
+	github.com/tus/tusd v1.3.0
+)
